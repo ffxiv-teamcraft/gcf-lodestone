006293f73e6870f5110696a5a56ac3c1a409cd7a
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2021-05-27 20:24:08 +1000

    Properly format ints

--- character.go
+++ character.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // GetCharacter fetches a Character by ID.
 func GetCharacter(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	if id == "" {
+	id, err := strconv.ParseUint(r.URL.Query().Get("id"), 10, 32)
+	if err != nil {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		log.Fatalln(err)
 	}
